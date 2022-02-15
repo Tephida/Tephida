c972fcc35b9685229a2e6837a4cf7eb922e69c28
Semen Alekseev <semyon492@ya.ru>
2022-02-15 11:27:12 +0300

    captcha

--- antibot/sec_code.php
+++ antibot/sec_code.php
@@ -10,20 +10,17 @@
 
 @error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
 
-function clean_url($url){
+function clean_url(string $url)
+{
+    $url = str_replace("http://", "", strtolower($url));
+    $url = str_replace("https://", "", $url);
+    if (str_starts_with($url, 'www.'))
+        $url = substr($url, 4);
+    $url = explode('/', $url);
+    $url = reset($url);
+    $url = explode(':', $url);
+    return reset($url);
 
-  if ($url == '') return;
-
-  $url = str_replace("http://", "", strtolower($url));
-  $url = str_replace("https://", "", $url );
-  if (substr($url, 0, 4) == 'www.')  $url = substr($url, 4);
-  $url = explode('/', $url);
-  $url = reset($url);
-  $url = explode(':', $url);
-  $url = reset($url);
-
-  return $url;
-  
 }
 
 if(clean_url($_SERVER['HTTP_REFERER']) != clean_url($_SERVER['HTTP_HOST'])) 
