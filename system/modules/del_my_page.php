cea8954b5060d9fe6f7efd9c68b75401818fd55e
Semen Alekseev <semyon492@ya.ru>
2022-03-06 19:29:45 +0300

    update

--- system/modules/del_my_page.php
+++ system/modules/del_my_page.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Copyright (c) 2022 Tephida
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ *
+ */
 
 use FluffyDollop\Support\{Filesystem, Registry};
 
