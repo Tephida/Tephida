812532ac66e83c80bd04740e9df22783d117c91c
Semen Alekseev <semyon492@ya.ru>
2022-04-13 23:14:06 +0300

    update

--- system/classes/Auth.php
+++ system/classes/Auth.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Copyright (c) 2022 Tephida
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ *
+ */
 
 declare(strict_types=1);
 
@@ -19,7 +26,6 @@ class Auth
     {
         $_IP = $_SERVER['REMOTE_ADDR'];
         $act = (new Request)->filter('act');
-//        $config = settings_get();
 //Если делаем выход
         if ($act === 'logout') {
             Cookie::remove('user_id');
